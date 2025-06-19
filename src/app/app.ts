801650bddf4b8be9d15a6c6d8354417f573d5cc8
Moritz Lüdtke <moritz.luedtke.contractor@draeger.com>
2025-06-19 17:26:50 +0200

    feat: added CPU benchmark

--- src/app/app.ts
+++ src/app/app.ts
@@ -14,6 +14,9 @@ export class App implements OnInit {
   public screenInfo: any = {};
   public batteryInfo: any = {};
 
+  loading = false;
+  result: number | null = null;
+
   ngOnInit() {
     this.ram = navigator.deviceMemory;
     this.cores = navigator.hardwareConcurrency;
@@ -34,5 +37,34 @@ export class App implements OnInit {
       viewportWidth: window.innerWidth,
       viewportHeight: window.innerHeight,
     };
+  }
+
+  runBenchmark(): void {
+    this.loading = true;
+    this.result = null;
+
+    setTimeout(() => {
+      const score = this.runCpuBenchmark(10_000_000);
+      this.result = parseFloat((100000 / score).toFixed(2));
+      this.loading = false;
+    });
+  }
+
+  private runCpuBenchmark(iterations: number): number {
+    const start = performance.now();
+    let count = 0;
+
+    for (let i = 2; i < iterations; i++) {
+      let isPrime = true;
+      for (let j = 2; j * j <= i; j++) {
+        if (i % j === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+      if (isPrime) count++;
+    }
+
+    return performance.now() - start;
   }
 }
