1598a5d4cfe63286fe0644d6c685e7b959a44799
Moritz Lüdtke <moritz.luedtke.contractor@draeger.com>
2025-06-19 14:56:17 +0200

    feat: added main functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   imports: [],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    
-  `,
-  styles: [],
+  templateUrl: "app.component.html",
+  styleUrl: "app.component.css",
 })
-export class App {
-  protected title = 'my-app';
+export class App implements OnInit {
+  public ram: number | undefined = 0;
+  public cores: number | undefined = 0;
+  public userAgent = "unknown";
+  public platform = "unknown";
+  public screenInfo: any = {};
+  public batteryInfo: any = {};
+
+  ngOnInit() {
+    this.ram = navigator.deviceMemory;
+
+    this.cores = navigator.hardwareConcurrency;
+
+    this.userAgent = navigator.userAgent;
+    this.platform = navigator.platform;
+
+    navigator.getBattery?.().then((battery: BatteryManager) => {
+      console.log(battery.level, battery.charging);
+      this.batteryInfo.level = battery.level;
+      this.batteryInfo.charging = battery.charging;
+    });
+
+    this.screenInfo = {
+      screenWidth: screen.width,
+      screenHeight: screen.height,
+      devicePixelRatio: window.devicePixelRatio,
+      viewportWidth: window.innerWidth,
+      viewportHeight: window.innerHeight,
+    };
+  }
 }
---
+++ src/typings.d.ts
@@ -0,0 +1,15 @@
+interface BatteryManager {
+  charging: boolean;
+  chargingTime: number;
+  dischargingTime: number;
+  level: number;
+  onchargingchange: ((this: BatteryManager, ev: Event) => any) | null;
+  onchargingtimechange: ((this: BatteryManager, ev: Event) => any) | null;
+  ondischargingtimechange: ((this: BatteryManager, ev: Event) => any) | null;
+  onlevelchange: ((this: BatteryManager, ev: Event) => any) | null;
+}
+
+interface Navigator {
+  getBattery?: () => Promise<BatteryManager>;
+  deviceMemory?: number;
+}
